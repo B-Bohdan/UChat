e3617da9304fed3ef90d3e2a7c6d2969f9396583
asazonov <Artem.Sazonov@cs.khpi.edu.ua>
2025-12-09 20:06:19 +0200

    Dialogs added

---
+++ uchat/Views/Dialogs/ErrorDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace uchat.Views.Dialogs
+{
+    public partial class ErrorDialog : Window
+    {
+        public ErrorDialog(string message, Window owner)
+        {
+            InitializeComponent();
+            this.Message = message;
+            this.Owner = owner;
+        }
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(ErrorDialog), new PropertyMetadata(string.Empty));
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
---
+++ uchat/Views/Dialogs/WarningDialog.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace uchat.Views.Dialogs
+{
+    public partial class WarningDialog : Window
+    {
+        public WarningDialog(string message, Window owner)
+        {
+            InitializeComponent();
+            this.Message = message;
+            this.Owner = owner;
+        }
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(WarningDialog), new PropertyMetadata(string.Empty));
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
